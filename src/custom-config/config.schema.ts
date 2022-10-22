ce9caa0644914642806cf0d3998a65ce72d8e9e1
Martin Dagarin <martin.dagarin@gmail.com>
2022-10-22 23:57:51 +0200

    Added OpenAPI

--- src/custom-config/config.schema.ts
+++ src/custom-config/config.schema.ts
@@ -1,6 +1,8 @@
 import * as Joi from 'joi';
 
 export const ConfigSchema: Joi.ObjectSchema = Joi.object().keys({
+	DOCS: Joi.boolean().default(true),
+	DOC_PATH: Joi.string().default('openapi'),
   NODE_ENV: Joi.string().valid('development', 'test', 'production').default('development'),
 	PORT: Joi.number().default(process.env.PORT || 3001),
 
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as compression from 'compression';
+import * as fs from 'fs';
 import helmet from 'helmet';
 
 import { AppModule } from './app.module';
+
+function getAppVersion(): string {
+  if (fs.existsSync('package.json')) {
+    try {
+      const pkg = JSON.parse(fs.readFileSync('package.json').toLocaleString());
+      return pkg.version || 'unknown';
+    } catch {
+      return 'unknown';
+    }
+  }
+  return 'unknown';
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
   // Get configuration
+  const docPath = configService.get<string>('DOC_PATH');
+  const enableDocs = configService.get<boolean>('DOCS');
   const port = configService.get<number>('PORT');
 
   // Middlewares
@@ -17,7 +33,19 @@ async function bootstrap() {
   app.use(helmet());
   app.enableCors();
   app.enableShutdownHooks();
-  
+
+  // API documentation
+  if (enableDocs) {
+    const docConfig = new DocumentBuilder()
+      .setTitle('Tip vozil')
+      .setDescription('Tip vozil API description')
+      .setVersion(getAppVersion())
+      // TODO: .addTag('tag', 'Title')
+      .build();
+    const document = SwaggerModule.createDocument(app, docConfig);
+    SwaggerModule.setup(docPath, app, document);
+  }
+
   await app.listen(port);
 }
 bootstrap();
